dd940a03ee4c125dc016d01b74eba1aa47017411
haithembel <haithemabderrahmen.belhadj@esprit.tn>
2018-10-02 14:55:58 +0200

    first commit

---
+++ MyFinance.Service/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using MyFinance.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFinance.Domain.Entities;
+using MyFinance.Data;
+using Service.Pattern;
+
+namespace MyFinance.Service
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private static IDatabaseFactory dbf =  new DatabaseFactory();
+        private static IUnitOfWork ut = new UnitOfWork(dbf);
+       
+        public ProductService()
+           : base(ut)
+        {
+           
+           
+
+        }
+        public IEnumerable<Product> GetProductsByCategory(string categoryName)
+        {
+            return ut.getRepository<Product>().GetMany(x => x.Category.Name == categoryName);
+        }
+
+
+
+
+
+    }
+
+}
---
+++ MyFinance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFinance.Startup))]
+namespace MyFinance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
